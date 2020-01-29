aff5a340812a5cbc1a4a3890c9f3ad5548b524c2
Alexander Rowland <arowland900@gmail.com>
2020-01-29 15:57:56 -0800

    add styling to homepage search results

--- controllers/index.js
+++ controllers/index.js
@@ -2,12 +2,23 @@ var Movie = require('../models/movie');
 var Song = require('../models/song');
 
 module.exports = {
-    index,
+    index
 };
 
 
 function index(req, res) {
-    if(!req.body.title) res.render('index', {title: "Search", movies: null, songs: null})
+
+    if (!req.body.title) {
+        if (req.method == "GET") {
+            res.render('index', { title: "Search", movies: null, songs: null })
+        } else {
+            Movie.find({}, function (err, movies) {
+                Song.find({}, function (err, songs) {
+                    res.render('index', { title: "Add Movie", movies, songs })
+                })
+            });
+        }
+    }
     Movie.find({ "title": { "$regex": req.body.title, "$options": "i" } }, function (err, movies) {
         Song.find({ "title": { "$regex": req.body.title, "$options": "i" } }, function (err, songs) {
             // let all = [...songs, movies].sort()
