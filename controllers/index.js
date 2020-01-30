a89bb416cad8e882b3b278d723504d328fe96d9b
Alexander Rowland <arowland900@gmail.com>
2020-01-29 16:50:57 -0800

    add sort functionality to backend for index.ejs results

--- controllers/index.js
+++ controllers/index.js
@@ -14,15 +14,26 @@ function index(req, res) {
         } else {
             Movie.find({}, function (err, movies) {
                 Song.find({}, function (err, songs) {
-                    res.render('index', { title: "Add Movie", movies, songs })
+                    // let all = [...movies, ...songs].sort((a,b) =>{
+                    //     console.log(a.title[0], b.title[0])
+                    //     return a.title[0] - b.title[0]
+                    // })
+                    let all = [...movies, ...songs].sort((a, b) => {
+                        if (a.title < b.title) { return -1; }
+                        if (a.title > b.title) { return 1; }
+                        return 0;
+                    })
+                    // console.log(all)
+                    res.render('index', { title: "Add Movie", movies, songs, all })
                 })
             });
         }
+    } else {
+        Movie.find({ "title": { "$regex": req.body.title, "$options": "i" } }, function (err, movies) {
+            Song.find({ "title": { "$regex": req.body.title, "$options": "i" } }, function (err, songs) {
+                // let all = [...songs, movies].sort()
+                res.render('index', { title: "Add Movie", movies, songs })
+            })
+        });
     }
-    Movie.find({ "title": { "$regex": req.body.title, "$options": "i" } }, function (err, movies) {
-        Song.find({ "title": { "$regex": req.body.title, "$options": "i" } }, function (err, songs) {
-            // let all = [...songs, movies].sort()
-            res.render('index', { title: "Add Movie", movies, songs })
-        })
-    });
 }
