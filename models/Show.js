f99b2218192e7c5712a29eddf37baa50a05f9ca3
Alexander Rowland <arowland900@gmail.com>
2020-02-03 10:58:01 -0800

    add Show schema

---
+++ models/Show.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+
+var showSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    season: String,
+    episode: {
+        epTitle: String;
+        num: Number
+    },
+    type: { type: String, default: 'song' }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Show', showSchema);
