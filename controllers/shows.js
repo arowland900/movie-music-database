c280fa3c6ce504ff58bd6c568cd6a7828ac72973
Alexander Rowland <arowland900@gmail.com>
2020-02-04 09:15:07 -0800

    add show controller

---
+++ controllers/shows.js
@@ -0,0 +1,35 @@
+var Show = require('../models/show');
+var Song = require('../models/song');
+
+module.exports = {
+    new: newMovie,
+    create,
+    show
+};
+
+
+function newShow(req, res) {
+    res.render('shows/new', { title: "Add Show" })
+}
+
+function show(req, res) {
+    Show
+        .findById(req.params.id)
+        .populate('songs episodes')
+        .exec(function (err, show) {
+            if (err) return res.redirect('/')
+            res.render('shows/detail', { show })
+        });
+}
+
+function create(req, res) {
+    for (let key in req.body) {
+        if (req.body[key] === '') delete req.body[key];
+    }
+    var show = new Show(req.body);
+    // movie.songs.push('5e3202ffadc67606612ac7c2')
+    show.save(function (err) {
+        if (err) return res.redirect('/shows/new');
+        res.redirect(`/`);
+    });
+}
---
+++ routes/shows.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+var showsCtrl = require('../controllers/shows')
+
+/* GET home page. */
+
+router.post('/', showsCtrl.create)
+router.get('/new', showsCtrl.new);
+router.get('/:id', showsCtrl.show);
+
+module.exports = router;
