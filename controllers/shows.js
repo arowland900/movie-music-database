6e6c4526d8359f59563eb0ec65d6fe61a9747c4b
Alexander Rowland <arowland900@gmail.com>
2020-02-04 09:34:13 -0800

    update shows/new.ejs & shows.js controller to take in creators string split by commas

--- controllers/shows.js
+++ controllers/shows.js
@@ -25,8 +25,9 @@ function create(req, res) {
     for (let key in req.body) {
         if (req.body[key] === '') delete req.body[key];
     }
+    req.body.creators = req.body.creators.split(',')
     var show = new Show(req.body);
-    
+
     // movie.songs.push('5e3202ffadc67606612ac7c2')
     show.save(function (err) {
         if (err) return res.redirect('/shows/new');
